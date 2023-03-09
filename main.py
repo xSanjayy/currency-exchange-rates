c21dcdef2a887f7cb6246bf3f57429d3d0a0dd43
adenosinetp10 <adenosinetp10@gmail.com>
2023-03-09 18:35:35 +0530

    PEP8 fix

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ conf = Config()
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(strings['START_MSG'].format(update.effective_user.first_name))
-    
+    await update.message.reply_text(Strings['START_MSG'].format(update.effective_user.first_name))
 
 
 if __name__ == '__main__':
-
     app = ApplicationBuilder().token(conf.BOT_TOKEN).build()
 
     app.add_handler(CommandHandler(
