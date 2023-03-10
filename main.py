a9b90d15a5079c2dcd0ffa9a1e43938dee56b91a
adenosinetp10 <adenosinetp10@gmail.com>
2023-03-10 09:05:52 +0530

    applied typing action wrapper and added `latest` command

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from strings import Strings
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import logging
 from telegram import Update
+from wrappers import send_typing_action
+from frankfurter import FrankFurter
+
+frank = FrankFurter()
 
 conf = Config()
 
@@ -12,8 +16,16 @@ logging.basicConfig(
 )
 
 
+@send_typing_action
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(Strings['START_MSG'].format(update.effective_user.first_name))
+
+
+@send_typing_action
+async def latest(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    res = frank.latest()
+
+    await update.message.reply_text(res)
 
 
 if __name__ == '__main__':
@@ -22,6 +34,11 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler(
         command='start',
         callback=start
+    ))
+
+    app.add_handler(CommandHandler(
+        command='latest',
+        callback=latest
     ))
 
     app.run_polling()
